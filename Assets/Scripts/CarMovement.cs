f0316bed60e66fbfed14b4e2ae012a85025d6ec2
Jordon Dodds <Jordon.Dodds@hotmail.ccom.au>
2016-04-05 15:46:21 +1000

    Initial commit with in class prototype

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CarMovement : MonoBehaviour {
+
+    public float movementSpeed = 20.0f;
+    public Rigidbody rb;
+    public Slider acceleration;
+    public Camera mainCamera;
+
+    private float carAcceleration = 0f;
+    //private float surfaceAsphalt = 0.9f;
+    //private float surfaceGrass = 0.3f;
+    //private GameObject asphalt;
+    //private GameObject grass;
+
+    // Use this for initialization
+    void Start ()
+    {
+    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Movement();
+        rb.AddForce(Vector3.left * movementSpeed * Time.deltaTime);
+        Debug.Log(movementSpeed);
+    }
+
+    void Movement ()
+    {
+        carAcceleration = acceleration.value;
+        movementSpeed += carAcceleration;
+    }
+}
---
+++ Assets/Scripts/Tags.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tags : MonoBehaviour
+{
+    public const string car = "Player";
+    public const string slider = "Slider";
+    public const string asphalt = "Asphalt";
+    public const string grass = "Grass";
+}

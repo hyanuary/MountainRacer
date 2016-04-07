6e053ad4eee034020a1276682e1e247375946829
Jordon Dodds <Jordon.Dodds@hotmail.ccom.au>
2016-04-07 15:53:47 +1000

    Slider bar working nicely

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 public class CarMovement : MonoBehaviour {
 
     public float movementSpeed = 20.0f;
-    public Rigidbody rb;
+    public float declerationRate = 10.0f;
     public Slider acceleration;
 
     //Wheels
@@ -14,29 +14,29 @@ public class CarMovement : MonoBehaviour {
     public WheelCollider backLeft;
     public WheelCollider backRight;
 
-    private float carAcceleration = 0f;
-
     // Use this for initialization
     void Start ()
     {
     
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         Movement();
+
+        if (acceleration.value <= 0 && movementSpeed > 0)
+        {
+            movementSpeed -= declerationRate;
+        }
     }
 
     void Movement ()
     {
-        //Car acceleration == slider position, mvement speed increaments by carAcceleration
-        carAcceleration = acceleration.value;
-        movementSpeed += carAcceleration;
+        movementSpeed += acceleration.value;
 
         //Apply force to rear wheels to simulate rear wheel drive
-        backLeft.motorTorque = movementSpeed;
-        backRight.motorTorque = movementSpeed;
-        Debug.Log(movementSpeed);
+        backLeft.motorTorque = Mathf.Clamp(movementSpeed, 0.0f, 500.0f) * Time.deltaTime;
+        backRight.motorTorque = Mathf.Clamp(movementSpeed, 0.0f, 500.0f) * Time.deltaTime;
     }
 }
